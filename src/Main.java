9c43c1176b81b638512e9a3e9b9139eca2739e07
jhmoon <sugaryfood@naver.com>
2022-12-14 13:05:51 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author 문지현
+ * @version 1.0
+ * @since ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/hackerRank/thirtyDaysOfCode/Day0HelloWorld.java
@@ -0,0 +1,34 @@
+package hackerRank.thirtyDaysOfCode;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+/**
+ * @author 문지현
+ * @version 1.0
+ * @since 2022-12-14
+ */
+public class Day0HelloWorld {
+
+    public static void main(String[] args) {
+        // Create a Scanner object to read input from stdin.
+        Scanner scan = new Scanner(System.in);
+
+        // Read a full line of input from stdin and save it to our variable, inputString.
+        String inputString = scan.nextLine();
+
+        // Close the scanner object, because we've finished reading
+        // all of the input from stdin needed for this challenge.
+        scan.close();
+
+        // Print a string literal saying "Hello, World." to stdout.
+        System.out.println("Hello, World.");
+
+        // TODO: Write a line of code here that prints the contents of inputString to stdout.
+        System.out.println(inputString);
+    }
+
+}
