105390dc227344022580ad93d11040ed837730e6
jhmoon <sugaryfood@naver.com>
2022-12-29 22:39:25 +0900

    Day12 : Inheritance

---
+++ src/hackerRank/thirtyDaysOfCode/Day12Inheritance.java
@@ -0,0 +1,85 @@
+package hackerRank.thirtyDaysOfCode;
+
+class Person {
+    protected String firstName;
+    protected String lastName;
+    protected int idNumber;
+
+    // Constructor
+    Person(String firstName, String lastName, int identification){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = identification;
+    }
+
+    // Print person data
+    public void printPerson(){
+        System.out.println(
+                "Name: " + lastName + ", " + firstName
+                        + 	"\nID: " + idNumber);
+    }
+
+}
+
+public class Day12Inheritance extends Person{
+    private int[] testScores;
+
+    /*
+     *   Class Constructor
+     *
+     *   @param firstName - A string denoting the Person's first name.
+     *   @param lastName - A string denoting the Person's last name.
+     *   @param id - An integer denoting the Person's ID number.
+     *   @param scores - An array of integers denoting the Person's test scores.
+     */
+    // Write your constructor here
+    public Day12Inheritance(String firstName, String lastName, int idNumber, int[] scores) {
+        super(firstName, lastName, idNumber);
+        this.testScores = scores;
+    }
+    /*
+     *   Method Name: calculate
+     *   @return A character denoting the grade.
+     */
+    // Write your method here
+    public char calculate() {
+        int sum = 0;
+        for (int i = 0; i < testScores.length; i++) {
+            sum += testScores[i];
+        }
+        int avg = sum/testScores.length;
+
+        if(90 <= avg && avg <= 100) {
+            return 'O';
+        } else if (80 <= avg) {
+            return 'E';
+        } else if (70 <= avg) {
+            return 'A';
+        } else if (50 <= avg) {
+            return 'P';
+        } else if (40 <= avg) {
+            return 'D';
+        } else {
+            return 'T';
+        }
+    }
+}
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String firstName = scan.next();
+        String lastName = scan.next();
+        int id = scan.nextInt();
+        int numScores = scan.nextInt();
+        int[] testScores = new int[numScores];
+        for(int i = 0; i < numScores; i++){
+            testScores[i] = scan.nextInt();
+        }
+        scan.close();
+
+        Day12Inheritance s = new Day12Inheritance(firstName, lastName, id, testScores);
+        s.printPerson();
+        System.out.println("Grade: " + s.calculate() );
+    }
+}
